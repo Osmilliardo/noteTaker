16abd2b20bcaca1a2df3af98882618091f5d3f97
Osmilliardo <140178301+Osmilliardo@users.noreply.github.com>
2024-01-01 13:11:50 -0500

    fixed bug with router

---
+++ db/notes.js
@@ -0,0 +1,54 @@
+const util = require('util');
+const fs = require('fs');
+
+const uuid = require('uuid/v1')
+
+const readAsync = util.promisify(fs.readFile);
+const writeAsync = util.promisify(fs.writeFile);
+
+class Save {
+    read() {
+        return readAsync('db/db.json', 'utf8');
+    }
+
+    write(notes) {
+        return writeAsync('db/db.json', JSON.stringify(notes, null, 2));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            }  catch (err) {
+                parsedNotes = [];
+            }
+
+            return parsedNotes;
+        });
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error('Neither the title or text fields can be blank');
+        }
+
+        const newNote = { title, text, id: uuid() };
+
+        return this.getNotes()
+        .then((notes) => [...notes, newNote])
+        .then((updated) => this.write(updated))
+        .then(() => newNote);
+    }
+
+    removeNote(id) {
+        return this.getNotes()
+        .then((notes) => notes.filter((note) => note.id !== id))
+        .then((filtered) => this.write(filtered));
+    }
+}
+
+module.exports = new Save();
---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const notes = require('../db/notes');
+
+router.get('/notes', (req, res) => {
+    notes
+    .getNotes()
+    .then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+    notes
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.delete('/notes/:id', (req, res) => {
+    notes
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
